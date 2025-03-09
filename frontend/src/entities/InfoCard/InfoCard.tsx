6f7292e40f635c252da702ed7cf26dcc63fe2b02
Артур Миров <amirov@mmk.local>
2025-03-09 13:32:42 +0700

    init

--- frontend/src/entities/InfoCard/InfoCard.tsx
+++ frontend/src/entities/InfoCard/InfoCard.tsx
@@ -1,12 +1,13 @@
 import {FC} from "react";
 import {InfoCardProps} from "@/shared/constants/constants";
+import styles from './InfoCard.module.scss';
 
 export const InfoCard: FC<InfoCardProps> = ({
     title,
     description
 }) => {
     return (
-        <div>
+        <div className={styles.whyCard}>
             {title}
             {description}
         </div>
