a83d6087aee49b2a540f25f424138d991b438355
Артур Миров <amirov@mmk.local>
2025-03-10 14:07:11 +0700

    init

--- frontend/src/widgets/Layout/Header/Header.tsx
+++ frontend/src/widgets/Layout/Header/Header.tsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router';
 import { Button } from '@/shared/ui';
 
 export const Header: FC = () => {
+  const profileUrl = `http://insight-estate.site:443/profile?basicToken=${localStorage.getItem('basicToken')}`
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -23,11 +24,11 @@ export const Header: FC = () => {
             Сформировать оффер
           </Link>
         </li>
-        {/*  <li className={styles.menu__item}>*/}
-        {/*    <Link className={styles.menu__link} to="faq">*/}
-        {/*      Ответы на вопросы*/}
-        {/*    </Link>*/}
-        {/*  </li>*/}
+        <li className={styles.menu__item}>
+          <Link className={styles.menu__link} to={profileUrl}>
+              Мой профиль
+          </Link>
+        </li>
         {/*  <li className={styles.menu__item}>*/}
         {/*    <Link className={styles.menu__link} to="contact">*/}
         {/*      Контакты*/}
