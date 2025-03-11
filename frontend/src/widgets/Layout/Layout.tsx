02f93e11c18fb64feca8acd968cfdf1cdfd1a9b7
Артур Миров <amirov@mmk.local>
2025-03-11 16:15:32 +0700

    init

--- frontend/src/widgets/Layout/Layout.tsx
+++ frontend/src/widgets/Layout/Layout.tsx
@@ -2,14 +2,16 @@ import {Outlet, useSearchParams} from 'react-router';
 import { Header } from './Header/Header';
 import { Footer } from './Footer/Footer';
 import styles from './Layout.module.scss';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const Layout = () => {
   const [searchParams, setSearchParams] = useSearchParams()
+  const [basicToken, setBasicToken] = useState<string | undefined | null>(localStorage.getItem('basicToken'))
   useEffect(() => {
       localStorage.setItem('basicToken', searchParams.get('basicToken') || localStorage.getItem('basicToken') || '');
+      setBasicToken(searchParams.get('basicToken') || localStorage.getItem('basicToken'))
+      console.log(basicToken)
   }, []);
-  const basicToken = localStorage.getItem('basicToken')
   return (
       <>
       {basicToken ?
