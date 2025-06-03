8e603c8d1a1f80a3317d0ac733600f6cacbc761a
ASBerezkin <tepmeh90694@yandex.ru>
2025-06-03 11:58:19 +0300

    FEAT - Fix

--- frontend/src/shared/ui/Pagination/Pagination.tsx
+++ frontend/src/shared/ui/Pagination/Pagination.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import styles from './Pagination.module.scss';
-import { useIntl } from 'react-intl';
 import MuiPagination, { PaginationProps as MUIPaginationProps } from '@mui/material/Pagination';
 
 type PaginationProps = {
@@ -15,28 +14,4 @@ export const Pagination: FC<PaginationProps> = ({ totalPages, pageNumber, onChan
       <MuiPagination count={totalPages} onChange={onChangePage} page={pageNumber} />
     </div>
   );
-
-  /*return (
-    <div className={styles.pagination}>
-      <div className={styles.pagination__buttons}>
-        {pageNumber !== 0 && (
-          <button className={styles.pagination__button} onClick={goToPreviousPage}>
-            {formatMessage({ id: 'pagination.back' })}
-          </button>
-        )}
-        {pageNumber !== totalPages && (
-          <button
-            className={styles.pagination__button}
-            onClick={goToNextPage}
-            disabled={pageNumber === totalPages}
-          >
-            {formatMessage({ id: 'pagination.next' })}
-          </button>
-        )}
-      </div>
-      <span className={styles.pagination__pages}>
-        {pageNumber + 1} / {totalPages}
-      </span>
-    </div>
-  );*/
 };
