583ce793abf3cfe24c0b494ac00887b84e306a32
Артур Миров <amirov@mmk.local>
2025-06-27 08:35:06 +0300

    init

--- frontend/src/widgets/Layout/Header/HeaderUnauth.tsx
+++ frontend/src/widgets/Layout/Header/HeaderUnauth.tsx
@@ -1,19 +1,12 @@
 import { FC } from 'react';
 import styles from './Header.module.scss';
 import { Dropdown } from '@/widgets/Dropdown/Dropdown';
-import { Link } from 'react-router';
-import { Logo } from '@/shared/assets/icons';
 
 export const HeaderUnauth: FC = () => {
   return (
     <>
-      <header className={styles.header}>
-        <div className={styles.logo}>
-          <Link to="/listing">
-            <Logo />
-          </Link>
-        </div>
-        <div className={styles.right}>
+      <header className={styles.header_unatuh}>
+        <div className={styles.right_unauth}>
           <Dropdown />
         </div>
       </header>
