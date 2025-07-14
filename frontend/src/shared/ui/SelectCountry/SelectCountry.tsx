451c397cd0d76cdeae1ea2829d3ede58f7f2adc3
ASBerezkin <tepmeh90694@yandex.ru>
2025-07-14 22:38:18 +0300

    FIX - All site. Part 1

--- frontend/src/shared/ui/SelectCountry/SelectCountry.tsx
+++ frontend/src/shared/ui/SelectCountry/SelectCountry.tsx
@@ -1,17 +1,30 @@
 import { ComponentProps, FC } from 'react';
-import { CountryDropdown } from 'react-country-region-selector';
+import { CountryDropdown, CountryRegionData } from 'react-country-region-selector';
 import styles from './SelectCountry.module.scss';
 import { Text } from '@/shared/ui';
+import { Autocomplete, TextField } from '@mui/material';
 
 type SelectCountryProps = ComponentProps<typeof CountryDropdown> & { label?: string };
 
-export const SelectCountry: FC<SelectCountryProps> = (props) => (
-  <div>
-    {props.label && (
-      <Text variant="heading4" as="label" className={styles.label}>
-        {props.label}
-      </Text>
-    )}
-    <CountryDropdown {...props} className={styles.select} />
-  </div>
-);
+const data = CountryRegionData.default.map(([name]) => name);
+
+export const SelectCountry: FC<SelectCountryProps> = (props) => {
+  return (
+    <Autocomplete
+      renderInput={(params) => <TextField {...params} label="" placeholder="Страна проживания" />}
+      options={data}
+      open
+    />
+  );
+
+  return (
+    <div>
+      {props.label && (
+        <Text variant="heading4" as="label" className={styles.label}>
+          {props.label}
+        </Text>
+      )}
+      <CountryDropdown {...props} className={styles.select} />
+    </div>
+  );
+};
