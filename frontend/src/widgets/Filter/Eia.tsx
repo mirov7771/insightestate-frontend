3bdc3b6df0a1b5166f86c06035a5b8c2f6c0c4ed
Артур Миров <amirov@mmk.local>
2025-12-22 16:46:07 +0300

    init

--- frontend/src/widgets/Filter/Eia.tsx
+++ frontend/src/widgets/Filter/Eia.tsx
@@ -1,9 +1,10 @@
 import { FC, useState } from 'react';
 import styles from './Filter.module.scss';
-import { Checkbox } from '@/shared/ui';
+import {Checkbox, Input} from '@/shared/ui';
 import { useFilters } from '@/widgets/Filter/model/useFilters';
 import { useIntl } from 'react-intl';
 import { useSearchParams } from 'react-router';
+import {FilterLayout} from "@/widgets/Filter/FilterLayout";
 
 export const Eia: FC = () => {
   const { formatMessage } = useIntl();
@@ -11,8 +12,8 @@ export const Eia: FC = () => {
   const [value, setValue] = useState(eia);
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const handleClick = () => {
-    setValue((prevState) => (prevState === 'true' ? 'false' : 'true'));
+  const handleClickYes = () => {
+    setValue((prevState) => (prevState === 'true' ? undefined : 'true'));
     setFilters((filtersState) => ({
       ...filtersState,
       pageNumber: 0,
@@ -24,14 +25,53 @@ export const Eia: FC = () => {
     });
   };
 
+  const handleClickNo = () => {
+    setValue((prevState) => (prevState === 'false' ? undefined : 'false'));
+    setFilters((filtersState) => ({
+      ...filtersState,
+      pageNumber: 0,
+      eia: filtersState.eia === 'false' ? undefined : 'false',
+    }));
+    setSearchParams((params) => {
+      params.set('page', '0');
+      return params;
+    });
+  };
+
+  const handleReset = () => {
+    setValue(undefined)
+    setFilters((filtersState) => ({
+      ...filtersState,
+      minPrice: 0,
+      eia: undefined,
+    }));
+    setSearchParams((params) => {
+      params.set('page', '0');
+      return params;
+    });
+  };
+
   return (
-    <div className={`${styles.content} ${styles.content__border}`}>
-      <Checkbox
-        name="eia"
-        onChange={handleClick}
-        checked={value === 'true'}
-        label={formatMessage({ id: 'eia' })}
+      <FilterLayout
+          name={`${formatMessage({ id: 'eia' })}`}
+          isActiveFilter={!!eia}
+          onResetFilter={handleReset}
+          filter={
+            <div className={styles.content}>
+              <Checkbox
+                  name="eiay"
+                  onChange={handleClickYes}
+                  checked={value === 'true'}
+                  label={formatMessage({ id: 'yes' })}
+              />
+              <Checkbox
+                  name="eian"
+                  onChange={handleClickNo}
+                  checked={value === 'false'}
+                  label={formatMessage({ id: 'no' })}
+              />
+            </div>
+          }
       />
-    </div>
   );
 };
--- frontend/src/widgets/Filter/LandPurchased.tsx
+++ frontend/src/widgets/Filter/LandPurchased.tsx
@@ -4,6 +4,7 @@ import { Checkbox } from '@/shared/ui';
 import { useFilters } from '@/widgets/Filter/model/useFilters';
 import { useIntl } from 'react-intl';
 import { useSearchParams } from 'react-router';
+import {FilterLayout} from "@/widgets/Filter/FilterLayout";
 
 export const LandPurchased: FC = () => {
   const { formatMessage } = useIntl();
@@ -11,8 +12,8 @@ export const LandPurchased: FC = () => {
   const [value, setValue] = useState(landPurchased);
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const handleClick = () => {
-    setValue((prevState) => (prevState === 'true' ? 'false' : 'true'));
+  const handleClickYes = () => {
+    setValue((prevState) => (prevState === 'true' ? undefined : 'true'));
     setFilters((filtersState) => ({
       ...filtersState,
       pageNumber: 0,
@@ -24,14 +25,53 @@ export const LandPurchased: FC = () => {
     });
   };
 
+  const handleClickNo = () => {
+    setValue((prevState) => (prevState === 'false' ? undefined : 'false'));
+    setFilters((filtersState) => ({
+      ...filtersState,
+      pageNumber: 0,
+      landPurchased: filtersState.landPurchased === 'false' ? undefined : 'false',
+    }));
+    setSearchParams((params) => {
+      params.set('page', '0');
+      return params;
+    });
+  };
+
+  const handleReset = () => {
+    setValue(undefined)
+    setFilters((filtersState) => ({
+      ...filtersState,
+      minPrice: 0,
+      landPurchased: undefined,
+    }));
+    setSearchParams((params) => {
+      params.set('page', '0');
+      return params;
+    });
+  };
+
   return (
-    <div className={`${styles.content} ${styles.content__border}`}>
-      <Checkbox
-        name="landPurchased"
-        onChange={handleClick}
-        checked={value === 'true'}
-        label={formatMessage({ id: 'landPurchased' })}
+      <FilterLayout
+          name={`${formatMessage({ id: 'landPurchased' })}`}
+          isActiveFilter={!!landPurchased}
+          onResetFilter={handleReset}
+          filter={
+            <div className={styles.content}>
+              <Checkbox
+                  name="landPurchasedy"
+                  onChange={handleClickYes}
+                  checked={value === 'true'}
+                  label={formatMessage({ id: 'yes' })}
+              />
+              <Checkbox
+                  name="landPurchasedn"
+                  onChange={handleClickNo}
+                  checked={value === 'false'}
+                  label={formatMessage({ id: 'no' })}
+              />
+            </div>
+          }
       />
-    </div>
   );
 };
